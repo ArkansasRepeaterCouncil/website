71a5458e1393b0adb358a54fc14749e3ba77b123
Joshua Carroll <joshcarroll21@gmail.com>
2018-03-29 21:26:32 -0500

    Initial commit

---
+++ repeaters/search/search.js
@@ -0,0 +1,12 @@
+var x = document.getElementById("repeaterList");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
