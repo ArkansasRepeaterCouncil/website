6f25fdea065316dce38bce6f36116186556a48c3
Joshua Carroll <joshcarroll21@gmail.com>
2018-06-13 14:08:37 -0500

    Initial commit

---
+++ App_Code/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Utilities
+/// </summary>
+public static class Utilities
+{
+	public static string Base64Encode(string plainText)
+	{
+		var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+		return System.Convert.ToBase64String(plainTextBytes);
+	}
+
+	public static string Base64Decode(string base64EncodedData)
+	{
+		var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+		return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+	}
+
+}
