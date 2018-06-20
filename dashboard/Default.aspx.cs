9ca2ae598e7fcb14844ed1dff52e43179faa7d6f
Joshua Carroll <joshcarroll21@gmail.com>
2018-06-20 16:45:01 -0500

    Initial commit

--- dashboard/Default.aspx.cs
+++ dashboard/Default.aspx.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class MasterPage : System.Web.UI.MasterPage
+public partial class dashboard_Default : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
