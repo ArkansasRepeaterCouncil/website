605757d990f04b4af709bc40699ee1578661983f
Joshua Carroll <joshcarroll21@gmail.com>
2018-08-29 17:18:12 -0500

    Changed auto patch to a string to account for none/open/closed

--- App_Code/Repeater.cs
+++ App_Code/Repeater.cs
@@ -58,7 +58,7 @@ public class Repeater
 	public string P25_NAC;
 	public string NXDN_RAN;
 	public string YSF_DSQ;
-	public bool Autopatch;
+	public string Autopatch;
 	public bool EmergencyPower;
 	public bool Linked;
 	public bool RACES;
@@ -99,7 +99,7 @@ public class Repeater
 		
 	}
 
-	public Repeater(string id, string type, string callsign, string trusteeid, string status, string city, string sitename, string outputfrequency, string inputfrequency, string sponsor, string latitude, string longitude, string amsl, string erp, string outputpower, string antennagain, string antennaheight, string analog_inputaccess, string analog_outputaccess, string analog_width, string dstar_module, string dmr_colorcode, string dmr_id, string dmr_network, string p25_nac, string nxdn_ran, string ysf_dsq, bool autopatch, bool emergencypower, bool linked, bool races, bool ares, bool widearea, bool weather, bool experimental, string datecoordinated, string dateupdated, string datedecoordinated, string datecoordinationsource, string dateconstruction, string coordinatorcomments, string notes, string state)
+	public Repeater(string id, string type, string callsign, string trusteeid, string status, string city, string sitename, string outputfrequency, string inputfrequency, string sponsor, string latitude, string longitude, string amsl, string erp, string outputpower, string antennagain, string antennaheight, string analog_inputaccess, string analog_outputaccess, string analog_width, string dstar_module, string dmr_colorcode, string dmr_id, string dmr_network, string p25_nac, string nxdn_ran, string ysf_dsq, string autopatch, bool emergencypower, bool linked, bool races, bool ares, bool widearea, bool weather, bool experimental, string datecoordinated, string dateupdated, string datedecoordinated, string datecoordinationsource, string dateconstruction, string coordinatorcomments, string notes, string state)
 	{
 		ID = int.Parse(id);
 		Type = type;
