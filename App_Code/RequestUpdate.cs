b1c03ad3460bb4388bce00f1e98e806007ef7744
Joshua Carroll <joshcarroll21@gmail.com>
2018-09-24 21:24:28 -0500

    Initial commit

---
+++ App_Code/RequestUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RequestUpdate
+/// </summary>
+public class RequestUpdate
+{
+	string UrlKey;
+	string statusId;
+	string note;
+
+	public RequestUpdate(string urlkey, string statusid, string Note)
+	{
+		UrlKey = urlkey;
+		statusId = statusid;
+		note = Note;
+	}
+
+	public void Save()
+	{
+		string url = System.Configuration.ConfigurationManager.AppSettings["webServiceRootUrl"] + "UpdateCoordinationRequestWorkflowStep";
+		string result = Utilities.PostJsonToUrl(url, this);
+	}
+}
