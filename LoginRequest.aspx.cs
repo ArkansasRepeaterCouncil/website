4b97c0703c3d50542d04657e6d092ecb57c0d6a5
Joshua Carroll <joshcarroll21@gmail.com>
2019-01-04 21:52:07 -0600

    Programmed code behind for LoginRequest.

--- LoginRequest.aspx.cs
+++ LoginRequest.aspx.cs
@@ -4,11 +4,66 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
 
 public partial class LoginRequest : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
 
+	}
+
+	protected void btnSubmit_Click(object sender, EventArgs e)
+	{
+		if (IsValid)
+		{
+			try
+			{
+				using (var webClient = new System.Net.WebClient())
+				{
+					string parameters = string.Format("callsign={0}&email={1}&name={2}&address={3}", txtCallsign.Text, txtEmail.Text, hdnName.Value, hdnAddress.Value);
+					string strUrl = string.Format("{0}{1}{2}", System.Configuration.ConfigurationManager.AppSettings["webServiceRootUrl"], "ResetPassword?", parameters);
+					string json = webClient.DownloadString(strUrl);
+					dynamic data = JsonConvert.DeserializeObject<dynamic>(json);
+				}
+
+				lblDirections.Text = "A new password has been created and queued to send to the email address we have on file. It may take up to 15 minutes before you receive it.";
+			}
+			catch (Exception ex)
+			{
+				new ExceptionReport(ex);
+				lblDirections.Text = "An error occurred while processing your reset request.  We have logged this error.  In the meantime you are welcome to browse to the home page, then back here to try again.";
+			}
+			btnSubmit.Enabled = false;
+			txtCallsign.Enabled = false;
+		}
+	}
+
+	protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+	{
+		// Look up the call sign and make sure it is a valid license
+		string response = Utilities.GetResponseFromUrl("https://callook.info/" + txtCallsign.Text + "/json");
+
+		try
+		{
+			dynamic data = JsonConvert.DeserializeObject(response);
+			if (data.status == "VALID")
+			{
+				args.IsValid = true;
+				hdnAddress.Value = data.address.line1;
+				hdnName.Value = data.name;
+			}
+			else
+			{
+				args.IsValid = false;
+			}
+		}
+		catch (Exception ex)
+		{
+			args.IsValid = false;
+
+			throw ex;
+		}
 	}
 }
